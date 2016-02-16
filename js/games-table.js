8350d33729079d5c55883390c309cf13b0d9c6e9
Mateusz Odelga <mateusz.odelga@schibsted.pl>
2016-02-16 12:32:48 +0100

    Add scrolling games

--- js/games-table.js
+++ js/games-table.js
@@ -1,12 +1,47 @@
-Vue.component("games-table",Vue.extend({
+Vue.component("games-table", Vue.extend({
   name: "games-table",
   template: $("#games-table-template").text(),
-  events:{
-    'on-data' : function(data){
-      this.$data = data;
+  scrollSize: 65,
+  visible: 5,
+  data: function() {
+    return {
+      currentScroll: 0,
+      games: {}
+    };
+  },
+  events: {
+    'on-data': function(data) {
+      this.games = data.games;
+      this.currentScroll = 0;
+      this._reScroll();
     }
   },
-  created:function(){
+  ready: function() {
+    this.embed = $(this.$el.nextSibling.parentElement).find('.games-scroll');
+    this.embed.height(this.height);
+  },
+  computed:{
+    height: function(){
+      return this.$options.visible * this.$options.scrollSize;
+    },
+    maxScrolled: function(){
+      return this.games.length - this.$options.visible;
+    }
+  },
+  methods: {
+    up: function() {
+        if(this.currentScroll !== 0){
+          this.currentScroll--;
+        }
+        this._reScroll();
+    },
+    down: function() {
+      if(this.currentScroll < this.maxScrolled)
+        this.currentScroll++;
+      this._reScroll();
+    },
+    _reScroll: function(){
+      this.embed.animate({scrollTop: this.$options.scrollSize * this.currentScroll});
+    }
   }
-
 }));
